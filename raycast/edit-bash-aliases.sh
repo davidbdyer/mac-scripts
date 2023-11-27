4374ee7a8aa08bf7fa0622220ae1ab40a55dab0f
davidbdyer <social_davidbdyer@icloud.com>
2023-11-27 18:41:00 -0500

    added two more scripts

---
+++ raycast/edit-bash-aliases.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Edit Bash Aliases
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName CLI Utils
+# @raycast.icon 📝
+
+# Documentation:
+# @raycast.description edit .bash_aliases file.
+# @raycast.author davidbdyer
+# @raycast.authorURL https://github.com/davidbdyer
+
+code ~/.bash_aliases
+
---
+++ raycast/edit-bash-profile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Edit Bash Profile
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.packageName CLI Utils
+# @raycast.icon 📝
+
+# Documentation:
+# @raycast.description edit bash profile
+# @raycast.author davidbddyer
+# @raycast.authorURL https://github.com/davidbdyer
+
+code ~/.bash_profile
+
