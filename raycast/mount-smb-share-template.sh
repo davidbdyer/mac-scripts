afc0bb97d335690327403552d6b69c6dd1ba8cdf
davidbdyer <social_davidbdyer@icloud.com>
2023-11-27 15:59:21 -0500

    added SMB template

---
+++ raycast/mount-smb-share-template.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Vault Multimedia
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🗄️
+
+# Documentation:
+# @raycast.description Mound a SMB share.
+# @author David Dyer
+# @authorURL https://www.daviddyer.me
+
+# username and password are optional.
+# It is not the best security practice to put your password in a plain text file.
+open smb://username:password@server-address
